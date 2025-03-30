03b2208fce4e532cea838bab4ae943d87813b0a4
Cayuga007 <123354205+Cayuga007@users.noreply.github.com>
2025-03-30 13:31:33 -0700

    transitions

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public Image black;
+    public Image white;
+    private float black_fade_duration = 2f;
+    private float white_fade_duration = 10f;
+    private int current_day;
+
+    private void Start()
+    {
+        current_day = int.Parse(SceneManager.GetActiveScene().name.Replace("day", ""));
+        LeanTween.alpha(black.gameObject, 0, black_fade_duration);
+    }
     public void LoadNextScene()
     {
-        int current_day = int.Parse(SceneManager.GetActiveScene().name.Replace("day", ""));
-
         if (current_day < 4)
         {
-            SceneManager.LoadScene("day" + (current_day + 1).ToString());
-        }
-        else
+            LeanTween.alpha(black.gameObject, 1, black_fade_duration).setOnComplete(NextDay);
+        } else
         {
-            Debug.Log("End of experience");
+            LeanTween.alpha(white.gameObject, 1, white_fade_duration).setOnComplete(NextDay);
         }
+    }
+
+    public void NextDay()
+    {
+        SceneManager.LoadScene("day" + (current_day + 1).ToString());
+    }
+
+    public void LastDay()
+    {
+        Application.Quit();
     }
 }
