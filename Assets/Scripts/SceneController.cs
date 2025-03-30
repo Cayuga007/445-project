31a97aa857a10c9e003c95e2c152ed30f6518065
Yuan Xiao <mgbai9954@gmail.com>
2025-03-30 13:58:41 -0700

    adjustments

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    public Image black;
-    public Image white;
+    public GameObject black;
+    public GameObject white;
     private float black_fade_duration = 2f;
     private float white_fade_duration = 10f;
     private int current_day;
@@ -15,16 +15,16 @@ public class SceneController : MonoBehaviour
     private void Start()
     {
         current_day = int.Parse(SceneManager.GetActiveScene().name.Replace("day", ""));
-        LeanTween.alpha(black.gameObject, 0, black_fade_duration);
+        LeanTween.alpha(black, 0, black_fade_duration);
     }
     public void LoadNextScene()
     {
         if (current_day < 4)
         {
-            LeanTween.alpha(black.gameObject, 1, black_fade_duration).setOnComplete(NextDay);
+            LeanTween.alpha(black, 1, black_fade_duration).setOnComplete(NextDay);
         } else
         {
-            LeanTween.alpha(white.gameObject, 1, white_fade_duration).setOnComplete(NextDay);
+            LeanTween.alpha(white, 1, white_fade_duration).setOnComplete(LastDay);
         }
     }
 
